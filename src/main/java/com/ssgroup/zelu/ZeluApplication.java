77a544ad57ee91a7517c1069b74211a07ffbeb4a
Clusters_stars <Clusters_stars@qq.com>
2023-10-26 20:51:20 +0800

    first commit

---
+++ src/main/java/com/ssgroup/zelu/ZeluApplication.java
@@ -0,0 +1,13 @@
+package com.ssgroup.zelu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeluApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssgroup/zelu/ZeluApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssgroup.zelu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
